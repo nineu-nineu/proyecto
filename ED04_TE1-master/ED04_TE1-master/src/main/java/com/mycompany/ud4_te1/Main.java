87daa2a9c7938a0dfb9bdf82adb131f9d4021c5f
nineu-nineu <84351989+nineu-nineu@users.noreply.github.com>
2021-05-18 14:01:17 +0200

    VERSION 2

---
+++ ED04_TE1-master/ED04_TE1-master/src/main/java/com/mycompany/ud4_te1/Main.java
@@ -0,0 +1,41 @@
+package com.mycompany.ud4_te1;
+
+/**
+ *
+ * @author Txomin
+ */
+public class Main {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        CCuenta cuentaTrabajo;
+        double saldoActual;
+
+        cuentaTrabajo = new CCuenta("Antonio López","1000-2365-85-1230456789", 2500,0);
+        saldoActual = cuentaTrabajo.getSaldo();
+        System.out.println("El saldo actual es " + saldoActual);
+        operativaCuenta(cuentaTrabajo, saldoActual);
+
+    }
+    public static void operativaCuenta(CCuenta cuentaTrabajo, double cantidad) {
+        
+        try {
+            System.out.println("Cargo en cuenta");
+            cuentaTrabajo.retirar(2300);
+            System.out.println("El nuevo saldo es: " + cuentaTrabajo.getSaldo());
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        
+        try {
+            System.out.println("Abono en cuenta");
+            cuentaTrabajo.ingresar(695);
+            System.out.println("El nuevo saldo es: " + cuentaTrabajo.getSaldo());
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
